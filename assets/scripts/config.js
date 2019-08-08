1081ef2a8f9e5c5290f83253f476c48a4aa7ede6
charholt <38872070+charholt@users.noreply.github.com>
2019-08-08 15:52:15 -0400

    Revert "Revert 5 show all events"

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://shielded-wildwood-50568.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
