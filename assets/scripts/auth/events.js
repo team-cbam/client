a650ac50b7d27f4443fd293925a4ec3355772db3
drekaygran <akgranbery@gmail.com>
2019-08-08 09:56:36 -0400

    Initiate authentication setup

---
+++ assets/scripts/auth/events.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const getFormFields = require(`../../../lib/get-form-fields`)
+const store = require('./../store')
+
+const api = require('./api')
+const ui = require('./ui')
+
+const onSignUp = event => {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  store.save = data
+  api.signUp(data)
+    .then(ui.signUpSuccess)
+    .catch(ui.signUpFailure)
+}
+
+const onSignIn = event => {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.signIn(data)
+    .then(ui.signInSuccess)
+    .catch(ui.signInFailure)
+}
+
+const onChangePassword = event => {
+  event.preventDefault()
+  const data = getFormFields(event.target)
+  api.changePassword(data)
+    .then(ui.changePasswordSuccess)
+    .catch(ui.changePasswordFailure)
+}
+
+const onSignOut = event => {
+  event.preventDefault()
+  api.signOut(event)
+    .then(ui.signOutSuccess)
+    .catch(ui.signOutFailure)
+}
+
+const addHandlers = () => {
+  $('#sign-up').on('submit', onSignUp)
+  $('#sign-in').on('submit', onSignIn)
+  $('#change-password').on('submit', onChangePassword)
+  $('#sign-out').on('click', onSignOut)
+}
+
+module.exports = {
+  addHandlers
+}
