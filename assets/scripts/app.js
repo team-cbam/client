28b80d9f5344811ca868d1b428b5ff64fa128716
charholt <charity.holt@icloud.com>
2019-08-08 11:52:45 -0400

    Add Shindigs Scripts  - CRUD shindigs  - Add event handers  - Add UI messages

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,10 +4,12 @@
 // const example = require('./example')
 const authEvents = require('./auth/events.js')
 const authUi = require('./auth/ui.js')
+const shingdigsEvents = require('./shindigs/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
   authUi.signedOut()
   authEvents.addHandlers()
+  shingdigsEvents.addHandlers()
 })
