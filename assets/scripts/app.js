f5c8d92258ec5bf08be5b2500e729ac5867a3ad2
britneyart80 <chen.bri@husky.neu.edu>
2019-08-08 18:11:02 -0400

    Reorganized handlebars for auth
    - removed handlebars for auth messages, now replaces auth-message
    directly
    - added classes 'invisible' to sections that do not show before sign in

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,13 +3,11 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events.js')
-const authUi = require('./auth/ui.js')
 const shingdigsEvents = require('./shindigs/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  authUi.signedOut()
   authEvents.addHandlers()
   shingdigsEvents.addHandlers()
 })
