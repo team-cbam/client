5b93c862c33ccaa8517e61b675db07feaf4a1d08
britneyart80 <chen.bri@husky.neu.edu>
2019-08-08 22:54:32 -0400

    Added request to get all events on page load
    - called method to load all events onto the page when loaded
    - removed header token from index action for events so that
    you can see events even if not logged in.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,11 +3,12 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 const authEvents = require('./auth/events.js')
-const shingdigsEvents = require('./shindigs/events.js')
+const shindigsEvents = require('./shindigs/events.js')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
+  shindigsEvents.onGetAllEvents()
   authEvents.addHandlers()
-  shingdigsEvents.addHandlers()
+  shindigsEvents.addHandlers()
 })
